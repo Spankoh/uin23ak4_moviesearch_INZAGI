cb4e46ba6d0a07ddad3cd98cc754a836d2a671c2
Johnny Inzagi <110547404+Spankoh@users.noreply.github.com>
2023-03-27 23:25:33 +0200

    Setup
    Setup for SASS, React and Bootstrap.

---
+++ components/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+      <input type="text" placeholder="Search..."></input>
+      <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
